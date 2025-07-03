68babe50cff8e9f7dd4d9875a143444bddd5606c
Claus Wilke <wilke@austin.utexas.edu>
2025-07-03 15:22:02 -0500

    rework correlation example

--- _site/datasets/correlation-matrix.R
+++ _site/datasets/correlation-matrix.R
@@ -3,27 +3,67 @@ library(here)
 library(colorspace)
 library(cowplot)
 
-# correlation data taken from
+# Data for correlation example taken from:
 # Spellman et al., Comprehensive identification of cell cycle-regulated
 # genes of the yeast Saccharomyces cerevisiae by microarray
 # hybridization. Mol. Biol. Cell, 1998
 # https://pubmed.ncbi.nlm.nih.gov/9843569/
 # PMCID: PMC25624
-# data is correlation coefficients between different yeast genes
-# across cellular states
-data <- 
-  read_csv("https://tidyplots.org/data/correlation-matrix.csv")
+# Available data is gene express profiles over time for thousands of yeast
+# genes. We calculate correlation coefficients between these expression profiles,
+# retaining only genes with few missing data values and that are on average
+# the most strongly correlated with other genes.
 
+# data downloaded from: https://pmc.ncbi.nlm.nih.gov/articles/instance/25624/bin/mbc_9_12_3273__CDCDATA.TXT
+df <- read_tsv(here("datasets", "mbc_9_12_3273__CDCDATA.txt")) |>
+  select(-`cln3 experiment1`, -`cln3 experiment2`, -`clb2 experiment2`, -`clb2 experiment1`) |>
+  rename(gene = `...1`) |>
+  pivot_longer(-gene, names_to = "experiment", values_to = "expr")
+
+# find genes with fewer than 6 missing values
+gene_list_low_na <- df |>
+  group_by(gene) |>
+  summarize(
+    na_count = sum(is.na(expr))
+  ) |>
+  filter(na_count < 6) |>
+  pull(gene)
+
+# retain only those genes
+df <- filter(df, gene %in% gene_list_low_na)
+
+# calculate correlation matrix
 cm <- df |>
-  select(x, y, correlation) |>
-  pivot_wider(names_from = x, values_from = correlation) |>
-  column_to_rownames("y") |>
-  as.matrix()
+  pivot_wider(names_from = "gene", values_from = "expr") |>
+  select(-experiment) |>
+  cor(use="pairwise.complete.obs")
 
-clust <- hclust(as.dist(1-cm), method="average") 
+# find genes with the highest average absolute correlations
+gene_list_high_cor <- as.data.frame(cm) |> 
+  rownames_to_column("gene") |>
+  pivot_longer(-gene, names_to = "gene2", values_to = "cor") |>
+  group_by(gene) |>
+  summarize(
+    meancor = mean(abs(cor))
+  ) |>
+  arrange(desc(meancor)) |>
+  slice(1:600) |>  # retain 600 top genes
+  pull(gene)
+
+# recalculate correlation matrix for top 600 genes from
+# previous analysis
+cm2 <- df |>
+  filter(gene %in% gene_list_high_cor) |>
+  pivot_wider(names_from = "gene", values_from = "expr") |>
+  select(-experiment) |>
+  cor(use="pairwise.complete.obs")
+
+clust <- hclust(as.dist(1-cm2), method="average") 
 levels <- clust$labels[clust$order]
 
-data_final <- data |>
+data_final <- as.data.frame(cm2) |>
+  rownames_to_column("x") |>
+  pivot_longer(-x, names_to = "y", values_to = "correlation") |>
   mutate(
     x = factor(x),
     y = factor(y),
@@ -36,9 +76,13 @@ data_final <- data |>
 
 saveRDS(data_final, here("datasets", "correlation-matrix.rds"))
 
+
+#-------------------------------------------------------
+# Plotting example
+
 make_plot <- function(data) {
   data |>
-  filter(as.integer(x) < as.integer(y)) |>
+    filter(as.integer(x) < as.integer(y)) |>
     ggplot(aes(x, y, fill = correlation)) + 
     geom_tile() +
     scale_x_discrete(
@@ -67,10 +111,11 @@ make_plot <- function(data) {
 
 data_final |>
   make_plot()
-  
+
 data_final |>
   mutate(
     x = x_ordered,
     y = y_ordered
   ) |>
   make_plot()
+
